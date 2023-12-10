ca593125ef48e7e9cb511736f63cee4a89ab6cc5
helpmehelpus <adrianortorres@gmail.com>
2023-12-10 17:41:58 +1030

    GET a file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-// Uncomment this block to pass the first stage
+use std::env::args;
+use std::fs;
 use std::net::{TcpListener, TcpStream};
 use std::io::{BufRead, BufReader, Write};
+use std::path::Path;
 use http_server_starter_rust::ThreadPool;
 
 const THREAD_POOL_SIZE: usize = 4;
 
 fn handle_connection(mut stream: TcpStream) {
+    let dir_name = args().nth(2).unwrap_or(String::from(""));
+
     let buf_reader = BufReader::new(&mut stream);
     let mut request = buf_reader.lines();
     let request_line = request.next().unwrap().unwrap();
@@ -33,6 +37,30 @@ fn handle_connection(mut stream: TcpStream) {
         stream
             .write(out.as_bytes())
             .expect("Unable to write to stream");
+    } else if path.starts_with("/files/") {
+        let file_name = path.strip_prefix("/files/").expect("missing filename in request");
+        if file_name.contains('/') {
+            let out = format!("HTTP/1.1 400 Bad Request\r\n\r\n");
+            stream
+                .write(out.as_bytes())
+                .expect("Unable to write to stream");
+        } else {
+            let full_path = dir_name + file_name;
+            let file_path = Path::new(full_path.as_str());
+            if file_path.exists() {
+                let file_contents = fs::read_to_string(&full_path).unwrap();
+                let length = file_contents.as_bytes().len();
+                let out = format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\n{}\r\n\r\n", length, file_contents);
+                stream
+                    .write(out.as_bytes())
+                    .expect("Unable to write to stream");
+            } else {
+                let out = format!("HTTP/1.1 404 Not Found\r\n\r\n");
+                stream
+                    .write(out.as_bytes())
+                    .expect("Unable to write to stream");
+            }
+        }
     } else {
         stream
             .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
@@ -44,8 +72,6 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     let pool = ThreadPool::new(THREAD_POOL_SIZE);
 
